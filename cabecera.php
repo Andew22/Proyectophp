1031097c16407796f505666c987a55982024f579
Andrew22 <agbuitrago2@misena.edu.co>
2022-04-26 17:05:12 -1000

    sistema de login simple utilizando variables de sesion en php

--- cabecera.php
+++ cabecera.php
@@ -1,3 +1,19 @@
+
+
+<?php
+
+session_start();
+
+if(isset($_SESSION['usuario'])!="admin"){
+
+
+    header("location:index.php");
+}
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- login.php
+++ login.php
@@ -1,69 +1,103 @@
+<?php
+
+
+session_start();
+
+if($_POST){
+
+        if($_POST['usuario']=="admin" && $_POST['password']="password"){
+
+
+        $_SESSION['usuario']="admin";
+
+
+        echo "logeado";
+
+        header("location:index.php");
+
+         
+
+        }else{
+
+        
+        echo "<script> alert('usuario o password incorrecta') </script>" ;
+        
+
+
+}
+
+}
+
+?>
+
 
 
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <title>Title</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-  </head>
-  <body>
+</head>
 
-
-<div class="container">
-
- <div class="row">
-     <div class="col-md-4">
-         
-     </div>
-
-     <div class="col-md-4">
-       
-     <div class="card">
-         <div class="card-header">
-             Iniciar Sesión
-         </div>
-         <div class="card-body">
-              <form action="login.php" method="post">
+<body>
 
 
-                <label  for="">Usuario :</label>
-                <input class="form-control" type="text" name="" id="">
+    <div class="container">
 
-                <label for="">Correo : </label>
-                <input class="form-control" type="text" name="" id="">
+        <div class="row">
+            <div class="col-md-4">
 
-                <br>
+            </div>
 
-                <button class="btn btn-success" type="submit">Entrar al portafolio</button>
+            <div class="col-md-4">
 
-
-            </form>
-         </div>
-         <div class="card-footer text-muted">
-             Footer
-         </div>
-     </div>
-         
-     </div>
-
-     <div class="col-md-4">
-         
-     </div>
-     
- </div>
+                <div class="card mt-5">
+                    <div class="card-header">
+                        Iniciar Sesión
+                    </div>
+                    <div class="card-body">
+                        <form action="login.php" method="post">
 
 
+                            <label for="">Usuario :</label>
+                            <input class="form-control"  name="usuario";  type="text" name="" id="">
 
-         
-     </div>
+                            <label for="">Password : </label>
+                            <input class="form-control"  name="password"  type="password" name="" id="">
+
+                            <br>
+
+                            <button class="btn btn-success" type="submit">Entrar al portafolio</button>
 
 
-    
-      
+                        </form>
+                    </div>
 
-   
-  </body>
+                </div>
+
+            </div>
+
+            <div class="col-md-4">
+
+            </div>
+
+        </div>
+
+
+
+
+    </div>
+
+
+
+
+
+
+</body>
+
 </html>
